e89d5159baec85ae3b3ee89c5a089d4645f4b05e
student <d3stud@kia.prz.edu.pl>
2025-04-11 23:57:25 +0200

    refactor(#27): removed unused import from tech details scraping script

--- RTV_tech_details.py
+++ RTV_tech_details.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from loguru import logger
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
