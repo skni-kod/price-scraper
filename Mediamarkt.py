47cbd37ea42ce0192771ca8648e99a17223c62d8
hlleric42 <patkoz04@o2.pl>
2025-04-23 21:30:27 +0200

    refactor: refactoring mediamarkt shop - Added function responsible for creating webdriver in config.py file - Translated comments from Polish to English - Integrated with config.py file - Improvements in detecting last page - num_of_opinions and rating_value are concatenated in one value - reviews

--- Mediamarkt.py
+++ Mediamarkt.py
@@ -1,69 +1,48 @@
-import os
+from config import(create_driver, date, setup_logging)
 import csv
-from datetime import datetime
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from loguru import logger
 import json
 
-# Tworzenie folderu output
-os.makedirs("output", exist_ok=True)
-
-# Ustawienia nazwy sklepu oraz daty
+# Define shop name
 shop_name = "mediamarkt"
-today = datetime.now().strftime("%Y-%m-%d")
-csv_filename = f"output/{shop_name}_{today}.csv"
-log_filename = f"output/log_{shop_name}_{today}.log"
 
-# Konfiguracja loggera
-logger.remove()  # Usuwa domyślne ustawienia
-logger.add(log_filename, level="INFO", format="{time} - {level} - {message}", encoding="utf-8")
-logger.add(lambda msg: print(msg, end=""), level="INFO", format="{time} - {level} - {message}")
+# Define CSV columns
+fieldnames = ["date","title", "product_link", "price", "image_url", "reviews"]
+# Define CSV filenames
+csv_filename = f"output/{shop_name}_{date}.csv"
+log_filename = f"output/log_{shop_name}_{date}.log"
 
-logger.info("Rozpoczęto scraping.")
-logger.info("Plik CSV: {}", csv_filename)
-logger.info("Plik logu: {}", log_filename)
+# Setup logger
+logger = setup_logging(log_filename)
+logger.info("Starting scraping.")
+logger.info("CSV File: {}", csv_filename)
+logger.info("Log File: {}", log_filename)
 
-# Konfiguracja przeglądarki Firefox
-firefox_binary_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-service = Service("geckodriver.exe")
-options = webdriver.FirefoxOptions()
-options.add_argument("--headless")
-options.binary_location = firefox_binary_path
-
-# Nagłówki kolumn w pliku CSV
-fieldnames = ["title", "product_link", "price", "num_of_opinions", "rating"]
-
-# Otwarcie pliku CSV do zapisu
+# Opening the CSV file for writing
 with open(csv_filename, mode="w", newline="", encoding="utf-8") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     page = 1
     while True:
-        # Inicjalizacja nowego webdrivera za każdym razem, bo driver.get(url) nie działa bo mediamarkt ma captche
-        driver = webdriver.Firefox(service=service, options=options)
-
+        driver = create_driver()
+        
         url = f"https://mediamarkt.pl/pl/category/smartfony-25983.html?page={page}"
-        logger.info(f"Przetwarzanie strony: {url}")
+        logger.info(f"Processing page: {url}")
 
-        # Otwórz stronę
         driver.get(url)
         driver.execute_script("document.body.style.transform = 'scale(0.3)'")
         try:
-            # Czekaj na załadowanie produktów
             WebDriverWait(driver, 15).until(
-                EC.presence_of_all_elements_located((By.XPATH, '//div[@data-test="mms-product-card"]'))
-            )
-
+                EC.presence_of_all_elements_located((By.XPATH, '//div[@data-test="mms-product-card"]')))
+          
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, "html.parser")
             json_scripts = soup.find_all('script', type='application/ld+json')
-
+            
             for script in json_scripts:
                 try:
                     json_data = json.loads(script.string)
@@ -72,31 +51,39 @@ with open(csv_filename, mode="w", newline="", encoding="utf-8") as csvfile:
                         for item in json_data.get('itemListElement', []):
                             product = item.get('item', {})
                             
-                            # Odczytanie danych
-                            name = product.get('name').replace("Smartfon ", "")
+                            # Reading the data
+                            title = product.get('name').replace("Smartfon ", "")
                             price = product.get('offers', {}).get('price')
                             rating_value = product.get('aggregateRating', {}).get('ratingValue')
-                            review_count = product.get('aggregateRating', {}).get('reviewCount')
-                            url = product.get('url')
+                            opinion_count = product.get('aggregateRating', {}).get('reviewCount')
+                            image_url = product.get('image')
+                            reviews = str(rating_value) + "/" + str(opinion_count)
+                            product_link = product.get('url')
                             
                             writer.writerow({
-                                "title": name,
-                                "product_link": url,
-                                "price": price,
-                                "num_of_opinions": review_count,
-                                "rating": rating_value,
-                            })
-                            logger.info(f"Zapisano produkt: {name}")
+                            "date": date,
+                            "title": title,
+                            "product_link": product_link,
+                            "price": price,
+                            "image_url": image_url,
+                            "reviews": reviews,
+                        })
+                            logger.info(f"Saved product: {title}")
+                            
                 except json.JSONDecodeError as e:
-                    logger.error(f"Błąd podczas parsowania JSON: {e}")
+                    logger.error(f"Error while parsing JSON: {e}")
                     continue
-            driver.quit()
+                
             page += 1
-
+            driver.quit()
+            button = soup.find('button', {'data-test': 'mms-search-srp-loadmore'})
+            if button == None:
+                logger.info("No more pages to scrape")
+                break
         except Exception as e:
-            logger.error(f"Błąd podczas przetwarzania strony {page}: {e}")
+            logger.error(f"Error during page processing {page}: {e}")
             driver.quit()
             break
 
-# Zamknij przeglądarkę po zakończeniu
-logger.info("Zakończono scraping.")
+# Close the browser when finished
+logger.info("Terminating script.")
--- config.py
+++ config.py
@@ -10,21 +10,21 @@ os.makedirs("output", exist_ok=True)
 # Date format
 date = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
-# Firefox and Geckodriver configuration
-options = webdriver.FirefoxOptions()
-options.add_argument("--headless")
-
 # TODO: Replace with docker later
 # Operating system detection
 system_os = platform.system()
-if system_os == "Windows":
-    firefox_binary_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-    options.binary_location = firefox_binary_path
-    service = Service("geckodriver.exe")
-else:
-    service = Service("/usr/local/bin/geckodriver")
 
-driver = webdriver.Firefox(service=service, options=options)
+def create_driver():
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+    if system_os == "Windows":
+        firefox_binary_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+        options.binary_location = firefox_binary_path
+        service = Service("geckodriver.exe")
+    else:
+        service = Service("/usr/local/bin/geckodriver")
+
+    return webdriver.Firefox(service=service, options=options)
 
 def setup_logging(log_filename):
     logger.remove()  # Deleting default handlers
